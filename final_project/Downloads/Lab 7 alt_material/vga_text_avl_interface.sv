0cda5f807382c439ab4a5f1dae9ef1adcbc605af
root <root@LAPTOP-RTDNEPAG>
2023-04-14 01:37:06 +0000

    second day of final

--- final_project/Downloads/Lab 7 alt_material/vga_text_avl_interface.sv
+++ final_project/Downloads/Lab 7 alt_material/vga_text_avl_interface.sv
@@ -46,38 +46,38 @@ module vga_text_avl_interface (
 	output logic [3:0]  red, green, blue,	// VGA color channels (mapped to output pins in top-level)
 	output logic hs, vs						// VGA HS/VS
 );
-logic [31:0] register_output, palette_output, drawing_output;
-//	ram x0(.address_a(AVL_ADDR), .address_b(char_addr), .byteena_a(AVL_BYTE_EN), .byteena_b(4'hF), .clock(CLK), .data_a(AVL_WRITEDATA), .data_b(0),
+//logic [31:0] register_output, palette_output, drawing_output;
+////	ram x0(.address_a(AVL_ADDR), .address_b(char_addr), .byteena_a(AVL_BYTE_EN), .byteena_b(4'hF), .clock(CLK), .data_a(AVL_WRITEDATA), .data_b(0),
+////		.rden_a(AVL_CS&&AVL_READ&&~AVL_ADDR[11]), .rden_b(1'b1), .wren_a(AVL_CS&&AVL_WRITE&&~AVL_ADDR[11]), .wren_b(1'b0), .q_a(register_output), .q_b(drawing_output));
+//
+//	ram2 x0(.address_a(AVL_ADDR), .address_b(char_addr), .byteena_a(AVL_BYTE_EN), .byteena_b(4'hF), .clock(CLK), .data_a(AVL_WRITEDATA), .data_b(0),
 //		.rden_a(AVL_CS&&AVL_READ&&~AVL_ADDR[11]), .rden_b(1'b1), .wren_a(AVL_CS&&AVL_WRITE&&~AVL_ADDR[11]), .wren_b(1'b0), .q_a(register_output), .q_b(drawing_output));
-
-	ram2 x0(.address_a(AVL_ADDR), .address_b(char_addr), .byteena_a(AVL_BYTE_EN), .byteena_b(4'hF), .clock(CLK), .data_a(AVL_WRITEDATA), .data_b(0),
-		.rden_a(AVL_CS&&AVL_READ&&~AVL_ADDR[11]), .rden_b(1'b1), .wren_a(AVL_CS&&AVL_WRITE&&~AVL_ADDR[11]), .wren_b(1'b0), .q_a(register_output), .q_b(drawing_output));
-//above is 7.2 logic
-
-logic [31:0] palette [8]; // palette registers
+////above is 7.2 logic
+//
+//logic [31:0] palette [8]; // palette registers
 
 //char_addr = (DrawX/8 + DrawY/16 * 80)
 //vramm = charaddr / 4
 //16*[7:0] + DrawY % 16 = addr
-always_comb
-begin
-if((AVL_ADDR[11]&&AVL_READ&&AVL_CS) == 1'b1)
-begin
-AVL_READDATA <= palette[AVL_ADDR[2:0]];
-end
-else
-begin
-AVL_READDATA <= register_output;
-end
-end
-
-always_ff@ (posedge CLK)
-begin
-if((AVL_ADDR[11]&&AVL_WRITE&&AVL_CS) == 1'b1)
-begin
-palette[AVL_ADDR[2:0]] = AVL_WRITEDATA;
-end
-end
+//always_comb
+//begin
+//if((AVL_ADDR[11]&&AVL_READ&&AVL_CS) == 1'b1)
+//begin
+//AVL_READDATA <= palette[AVL_ADDR[2:0]];
+//end
+//else
+//begin
+//AVL_READDATA <= register_output;
+//end
+//end
+//
+//always_ff@ (posedge CLK)
+//begin
+//if((AVL_ADDR[11]&&AVL_WRITE&&AVL_CS) == 1'b1)
+//begin
+//palette[AVL_ADDR[2:0]] = AVL_WRITEDATA;
+//end
+//end
 
 logic [9:0] X, Y;
 logic [6:0] address;
@@ -85,8 +85,8 @@ logic [7:0] font_data;
 logic pixel_clk, blank;
 
 vga_controller vga_0 (.Clk(CLK), .Reset(RESET), .hs(hs), .vs(vs), .blank(blank), .pixel_clk(pixel_clk), .DrawX(X), .DrawY(Y));
-font_rom rom(.addr(addressinput), .data(font_data));
-
+//font_rom rom(.addr(addressinput), .data(font_data));
+dogactualfinal_example example(.DrawX(X), .DrawY(Y),.vga_clk(CLK), .blank(blank),.red(red), .green(green), .blue(blue));
 // Read and write from AVL interface to register block, note that READ waitstate = 1, so this should be in always_ff
 //always_ff @(posedge CLK) 
 //begin
@@ -121,116 +121,62 @@ font_rom rom(.addr(addressinput), .data(font_data));
 //end
 //
 //end
-logic [10:0]char_addr;
-logic [2:0] palette_idx_fgd, palette_idx_bgd;
-logic [3:0] palette_red_fgd, palette_green_fgd, palette_blue_fgd, palette_red_bgd, palette_green_bgd, palette_blue_bgd;
-//logic [9:0] vram;
-logic inverse_bit;
-logic [10:0] addressinput;
-assign addressinput = address*16 + Y[3:0];
-assign char_addr = (X[9:4] + Y[9:4]*40);
-//divide by 2 to get register for sprite(7.2)
-//handle drawing (may either be combinational or sequential - or both).
-always_comb
-begin
-inverse_bit = 0;
-address = 0;
-palette_idx_fgd = 0;
-palette_idx_bgd = 0;
-palette_red_fgd = 0;
-palette_blue_fgd = 0;
-palette_green_fgd = 0;
-palette_red_bgd = 0;
-palette_green_bgd = 0;
-palette_blue_bgd = 0;
-
-//row major order to get pixel count, divide by four to get register index
-if((X[3]) == 1'b0) //dividing by 16 (7.2) to get 16 bit sprite block and then modding (by 2 for 7.2) to get exact block
-begin
-address = (drawing_output[14:8]); //16*register value + DrawY mod 16 to find row of data in font_rom
-inverse_bit = drawing_output[15];
-palette_idx_fgd = drawing_output[7:5]; //fgd [7:4] /2
-palette_idx_bgd = drawing_output[3:1]; //bgd	[3:0] /2
-if(drawing_output[4] == 1)
-begin
-palette_red_fgd = palette[palette_idx_fgd][24:21];
-palette_green_fgd = palette[palette_idx_fgd][20:17];
-palette_blue_fgd = palette[palette_idx_fgd][16:13];
-end
-else
-begin
-palette_red_fgd = palette[palette_idx_fgd][12:9];
-palette_green_fgd = palette[palette_idx_fgd][8:5];
-palette_blue_fgd = palette[palette_idx_fgd][4:1];
-end
-if(drawing_output[0] == 1)
-begin
-palette_red_bgd = palette[palette_idx_bgd][24:21];
-palette_green_bgd = palette[palette_idx_bgd][20:17];
-palette_blue_bgd = palette[palette_idx_bgd][16:13];
-end
-else
-begin
-palette_red_bgd = palette[palette_idx_bgd][12:9];
-palette_green_bgd = palette[palette_idx_bgd][8:5];
-palette_blue_bgd = palette[palette_idx_bgd][4:1];
-end
- 
-//	if(drawing_output[7] == 1'b1)
-//	begin
-//	real_font_data = ~font_data;
-//	end
-//	else
-//	begin
-//	real_font_data = font_data;
-//	end
-end
-
-else if((X[3]) == 1'b1) //dividing by 16 to get 16 bit sprite block and then modding to get exact block
-begin
-address = (drawing_output[30:24]); //16*register value + DrawY mod 16 to find row of data in font_rom
-inverse_bit = drawing_output[31];
-palette_idx_fgd = drawing_output[23:21]; //fgd
-palette_idx_bgd = drawing_output[19:17]; //bgd
-if(drawing_output[20] == 1)
-begin
-palette_red_fgd = palette[palette_idx_fgd][24:21];
-palette_green_fgd = palette[palette_idx_fgd][20:17];
-palette_blue_fgd = palette[palette_idx_fgd][16:13];
-end
-else
-begin
-palette_red_fgd = palette[palette_idx_fgd][12:9];
-palette_green_fgd = palette[palette_idx_fgd][8:5];
-palette_blue_fgd = palette[palette_idx_fgd][4:1];
-end
-if(drawing_output[16] == 1)
-begin
-palette_red_bgd = palette[palette_idx_bgd][24:21];
-palette_green_bgd = palette[palette_idx_bgd][20:17];
-palette_blue_bgd = palette[palette_idx_bgd][16:13];
-end
-else
-begin
-palette_red_bgd = palette[palette_idx_bgd][12:9];
-palette_green_bgd = palette[palette_idx_bgd][8:5];
-palette_blue_bgd = palette[palette_idx_bgd][4:1];
-end
-//	if(drawing_output[15] == 1'b1)
-//	begin
-//	real_font_data = ~font_data;
-//	end
-//	else
-//	begin
-//	real_font_data = font_data;
-//	end
-end
-
-//else if((X[4:3]) == 2'b10) //dividing by 8 to get 8 bit sprite block and then modding to get exact block
+//logic [10:0]char_addr;
+//logic [2:0] palette_idx_fgd, palette_idx_bgd;
+//logic [3:0] palette_red_fgd, palette_green_fgd, palette_blue_fgd, palette_red_bgd, palette_green_bgd, palette_blue_bgd;
+////logic [9:0] vram;
+//logic inverse_bit;
+//logic [10:0] addressinput;
+//assign addressinput = address*16 + Y[3:0];
+//assign char_addr = (X[9:4] + Y[9:4]*40);
+////divide by 2 to get register for sprite(7.2)
+////handle drawing (may either be combinational or sequential - or both).
+//always_comb
 //begin
-//address = (drawing_output[22:16]); //16*register value + DrawY mod 16 to find row of data in font_rom
-//inverse_bit = drawing_output[23];
-////	if(drawing_output[23] == 1'b1)
+//inverse_bit = 0;
+//address = 0;
+//palette_idx_fgd = 0;
+//palette_idx_bgd = 0;
+//palette_red_fgd = 0;
+//palette_blue_fgd = 0;
+//palette_green_fgd = 0;
+//palette_red_bgd = 0;
+//palette_green_bgd = 0;
+//palette_blue_bgd = 0;
+//
+////row major order to get pixel count, divide by four to get register index
+//if((X[3]) == 1'b0) //dividing by 16 (7.2) to get 16 bit sprite block and then modding (by 2 for 7.2) to get exact block
+//begin
+//address = (drawing_output[14:8]); //16*register value + DrawY mod 16 to find row of data in font_rom
+//inverse_bit = drawing_output[15];
+//palette_idx_fgd = drawing_output[7:5]; //fgd [7:4] /2
+//palette_idx_bgd = drawing_output[3:1]; //bgd	[3:0] /2
+//if(drawing_output[4] == 1)
+//begin
+//palette_red_fgd = palette[palette_idx_fgd][24:21];
+//palette_green_fgd = palette[palette_idx_fgd][20:17];
+//palette_blue_fgd = palette[palette_idx_fgd][16:13];
+//end
+//else
+//begin
+//palette_red_fgd = palette[palette_idx_fgd][12:9];
+//palette_green_fgd = palette[palette_idx_fgd][8:5];
+//palette_blue_fgd = palette[palette_idx_fgd][4:1];
+//end
+//if(drawing_output[0] == 1)
+//begin
+//palette_red_bgd = palette[palette_idx_bgd][24:21];
+//palette_green_bgd = palette[palette_idx_bgd][20:17];
+//palette_blue_bgd = palette[palette_idx_bgd][16:13];
+//end
+//else
+//begin
+//palette_red_bgd = palette[palette_idx_bgd][12:9];
+//palette_green_bgd = palette[palette_idx_bgd][8:5];
+//palette_blue_bgd = palette[palette_idx_bgd][4:1];
+//end
+// 
+////	if(drawing_output[7] == 1'b1)
 ////	begin
 ////	real_font_data = ~font_data;
 ////	end
@@ -240,11 +186,37 @@ end
 ////	end
 //end
 //
-//else if((X[4:3]) == 2'b11) //dividing by 8 to get 8 bit sprite block and then modding to get exact block
+//else if((X[3]) == 1'b1) //dividing by 16 to get 16 bit sprite block and then modding to get exact block
 //begin
 //address = (drawing_output[30:24]); //16*register value + DrawY mod 16 to find row of data in font_rom
 //inverse_bit = drawing_output[31];
-////	if(drawing_output[31] == 1'b1)
+//palette_idx_fgd = drawing_output[23:21]; //fgd
+//palette_idx_bgd = drawing_output[19:17]; //bgd
+//if(drawing_output[20] == 1)
+//begin
+//palette_red_fgd = palette[palette_idx_fgd][24:21];
+//palette_green_fgd = palette[palette_idx_fgd][20:17];
+//palette_blue_fgd = palette[palette_idx_fgd][16:13];
+//end
+//else
+//begin
+//palette_red_fgd = palette[palette_idx_fgd][12:9];
+//palette_green_fgd = palette[palette_idx_fgd][8:5];
+//palette_blue_fgd = palette[palette_idx_fgd][4:1];
+//end
+//if(drawing_output[16] == 1)
+//begin
+//palette_red_bgd = palette[palette_idx_bgd][24:21];
+//palette_green_bgd = palette[palette_idx_bgd][20:17];
+//palette_blue_bgd = palette[palette_idx_bgd][16:13];
+//end
+//else
+//begin
+//palette_red_bgd = palette[palette_idx_bgd][12:9];
+//palette_green_bgd = palette[palette_idx_bgd][8:5];
+//palette_blue_bgd = palette[palette_idx_bgd][4:1];
+//end
+////	if(drawing_output[15] == 1'b1)
 ////	begin
 ////	real_font_data = ~font_data;
 ////	end
@@ -253,33 +225,61 @@ end
 ////	real_font_data = font_data;
 ////	end
 //end
-
-end
-
-always_ff@(posedge pixel_clk)
-begin
-
-if(~blank)
-begin
-red <= 4'h0;
-green <= 4'h0;
-blue <= 4'h0; 
-end
-else if ( (inverse_bit)^(font_data[7-X[2:0]]) == 1'b1)
-begin
-red <= palette_red_fgd;
-green <= palette_green_fgd;
-blue <= palette_blue_fgd; 
-end
-else 
-begin
-red <= palette_red_bgd;
-green <= palette_green_bgd;
-blue <= palette_blue_bgd; 
-end
-
-
-end
+//
+////else if((X[4:3]) == 2'b10) //dividing by 8 to get 8 bit sprite block and then modding to get exact block
+////begin
+////address = (drawing_output[22:16]); //16*register value + DrawY mod 16 to find row of data in font_rom
+////inverse_bit = drawing_output[23];
+//////	if(drawing_output[23] == 1'b1)
+//////	begin
+//////	real_font_data = ~font_data;
+//////	end
+//////	else
+//////	begin
+//////	real_font_data = font_data;
+//////	end
+////end
+////
+////else if((X[4:3]) == 2'b11) //dividing by 8 to get 8 bit sprite block and then modding to get exact block
+////begin
+////address = (drawing_output[30:24]); //16*register value + DrawY mod 16 to find row of data in font_rom
+////inverse_bit = drawing_output[31];
+//////	if(drawing_output[31] == 1'b1)
+//////	begin
+//////	real_font_data = ~font_data;
+//////	end
+//////	else
+//////	begin
+//////	real_font_data = font_data;
+//////	end
+////end
+//
+//end
+//
+//always_ff@(posedge pixel_clk)
+//begin
+//
+//if(~blank)
+//begin
+//red <= 4'h0;
+//green <= 4'h0;
+//blue <= 4'h0; 
+//end
+//else if ( (inverse_bit)^(font_data[7-X[2:0]]) == 1'b1)
+//begin
+//red <= palette_red_fgd;
+//green <= palette_green_fgd;
+//blue <= palette_blue_fgd; 
+//end
+//else 
+//begin
+//red <= palette_red_bgd;
+//green <= palette_green_bgd;
+//blue <= palette_blue_bgd; 
+//end
+//
+//
+//end
 
 
 endmodule
